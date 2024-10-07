e2e35b0391e325f3757c47c5272fdcce85007f56
AustineDavid <davmesoaustine@gmail.com>
2024-10-07 21:18:41 +0100

    make sending mails work

---
+++ controllers/resetpassword.js
@@ -0,0 +1,47 @@
+import nodemailer from "nodemailer";
+import hbs from "nodemailer-express-handlebars";
+
+export const ResetPassword = async (req, res) => {
+  const { name, link, email } = req.body;
+  try {
+    // Gmail SMTP transporter configuration
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "austinedavid96@gmail.com",
+        pass: process.env.Gmailpassword, // Use an app-specific password
+      },
+    });
+
+    // handlebars option
+    const handlebarOptions = {
+      viewEngine: {
+        defaultLayout: false,
+      },
+      viewPath: "views",
+    };
+
+    transporter.use("compile", hbs(handlebarOptions));
+
+    const messageObject = {
+      from: "austinedavid96@gmail.com", // sender address
+      to: [email], // list of receivers
+      subject: "SchooledAfrika Reset Password", // Subject line
+      template: "resetpass",
+      context: {
+        name,
+        link,
+      },
+    };
+
+    transporter.sendMail(messageObject, (error, info) => {
+      if (error) {
+        return res.status(404).json({ message: error.message });
+      }
+      return res.status(200).json({ message: info.messageId });
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Failed to send email" });
+  }
+};
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import { ResetPassword } from "./controllers/resetpassword.js";
 dotenv.config();
-import nodemailer from "nodemailer";
-import hbs from "nodemailer-express-handlebars";
 
 const app = express();
 
@@ -10,52 +9,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/war", (req, res) => {
-  res.send("hello world");
-});
-app.post("/message", async (req, res) => {
-  const { name, email } = req.body;
-  try {
-    // Gmail SMTP transporter configuration
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "austinedavid96@gmail.com",
-        pass: process.env.Gmailpassword, // Use an app-specific password
-      },
-    });
-
-    // handlebars option
-    const handlebarOptions = {
-      viewEngine: {
-        defaultLayout: false,
-      },
-      viewPath: "views",
-    };
-
-    transporter.use("compile", hbs(handlebarOptions));
-
-    const messageObject = {
-      from: "austinedavid96@gmail.com", // sender address
-      to: [email], // list of receivers
-      subject: "Hello ✔", // Subject line
-      template: "resetpass",
-      context: {
-        name,
-      },
-    };
-
-    transporter.sendMail(messageObject, (error, info) => {
-      if (error) {
-        return res.status(404).json({ message: error.message });
-      }
-      return res.status(200).json({ message: info.messageId });
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Failed to send email" });
-  }
-});
+app.post("/message", ResetPassword);
 
 app.listen(PORT, () => {
   console.log("app is running now");
