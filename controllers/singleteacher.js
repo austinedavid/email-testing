928a9618cb3cd7400c8cee06fd66d29ae9128697
AustineDavid <davmesoaustine@gmail.com>
2024-11-21 05:26:11 +0100

    allow sending mails for single user

---
+++ controllers/singleteacher.js
@@ -0,0 +1,46 @@
+import nodemailer from "nodemailer";
+import hbs from "nodemailer-express-handlebars";
+
+export const SingleMessage = async (req, res) => {
+  const { subject, message, email } = req.body;
+  try {
+    // Gmail SMTP transporter configuration
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "austinedavid96@gmail.com",
+        pass: process.env.Gmailpassword, // Use an app-specific password
+      },
+    });
+
+    // handlebars option
+    const handlebarOptions = {
+      viewEngine: {
+        defaultLayout: false,
+      },
+      viewPath: "views",
+    };
+
+    transporter.use("compile", hbs(handlebarOptions));
+
+    const messageObject = {
+      from: "austinedavid96@gmail.com", // sender address
+      to: [email], // list of receivers
+      subject, // Subject line
+      template: "singleteacher",
+      context: {
+        message,
+      },
+    };
+
+    transporter.sendMail(messageObject, (error, info) => {
+      if (error) {
+        return res.status(404).json({ message: error.message });
+      }
+      return res.status(200).json({ message: info.messageId });
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Failed to send email" });
+  }
+};
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { ResetPassword } from "./controllers/resetpassword.js";
 dotenv.config();
+import { SingleMessage } from "./controllers/singleteacher.js";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 4000;
 
 app.post("/message", ResetPassword);
+app.post("/message-to-singleTeacher", SingleMessage);
 
 app.listen(PORT, () => {
   console.log("app is running now");
